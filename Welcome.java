39355ab32249e421e9fb6d23de517b0116f8ed21
Andrew Holcomb <atholcomb@outlook.com>
2024-07-21 21:51:38 -0600

    Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+/*
+// written by: atholcomb
+// Welcome.java
+// Welcome program to greet user
+*/
+
+public class Welcome {
+  public static void main(String[] args) {
+    System.out.println("Welcome to IterLabs");
+  }
+}
